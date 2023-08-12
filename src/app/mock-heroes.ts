96c4bc7a93193d4b982e07f868b6d3b280892044
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-12 18:28:16 +0530

    2 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,6 +4,6 @@ export const HEROES:Hero[]=[
   {id:1,avatar:"https://www.forbesindia.com/media/images/2022/Aug/img_192351_viratkohli.jpg",name:'Virat Kohli',field:'Cricket',info:''},
   {id:2,avatar:"https://filmfare.wwmindia.com/content/2018/mar/600by450ab15174803041521323491.jpg",name:'Amitabh Bachann',field:'Film star',info:''},
   {id:3,avatar:"https://static.toiimg.com/photo/msid-101150151/101150151.jpg",name:'Narendra Modi',field:'Politics',info:''},
-  {id:4,avatar:"https://imgeng.jagran.com/images/2023/may/sundarpichai1683786929944.jpg",name:'Sundar Pichai',field:'Material science and design',info:''},
+  {id:4,avatar:"https://cdn.britannica.com/42/193142-050-F69B1A23/Sundar-Pichai-Google.jpg",name:'Sundar Pichai',field:'Material science and design',info:''},
   {id:5,avatar:"https://www.geo.tv/assets/uploads/updates/2023-05-05/485628_6197077_updates.jpg",name:'Arjit singh',field:'Singing',info:'swing Master'},
 ];
