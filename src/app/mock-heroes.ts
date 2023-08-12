3de6e483f9ba926ae9497d4df8e906ec026c5f3e
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-12 11:17:05 +0530

    1 file modified, 5 added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import{Hero} from "./hero";
+
+export const HEROES:Hero[]=[
+  {id:1,avatar:"https://www.forbesindia.com/media/images/2022/Aug/img_192351_viratkohli.jpg",name:'Virat Kohli',field:'Cricket',info:'Run Machine'},
+  {id:1,avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyrC8CgbadnAb21D-RcmqFxI_ICLtR31tWew&usqp=CAU",name:'Rohit Sharma',field:'Cricket',info:'Hitman'},
+  {id:1,avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6MXuJ3KvpWi85FrAhgRc3ThKmQtWU3fECsQ&usqp=CAU",name:'M.S.Dhoni',field:'Cricket',info:'Captain Cool'},
+  {id:1,avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUYadjec_YOf4d2s_G4R9yp_zQ68AJ3HkpJQ&usqp=CAU",name:'Bumrah',field:'Cricket',info:'Yorker king'},
+  {id:1,avatar:"https://static.toiimg.com/thumb/msid-69959794,width-400,resizemode-4/69959794.jpg",name:'Bhuvi',field:'Cricket',info:'swing Master'},
+];
