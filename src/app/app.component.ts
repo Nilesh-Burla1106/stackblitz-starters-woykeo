752f436e6b7844f8b30a8260c75b5a72eca7da42
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-12 11:32:37 +0530

    2 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   standalone:true,
   selector: 'app-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[HeroesComponent,HeroDetailsComponent]
 })
 export class AppComponent implements OnInit {
 
